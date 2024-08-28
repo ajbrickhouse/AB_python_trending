3b9e84ddedfd83fc9c11685e2f06a91d3ed0a593
ajbrickhouse <ajbrinkhuis@gmail.com>
2024-08-28 14:09:04 -0700

    chore: Refactor Flask app for PLC trend collection control

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import threading
 import time
 import datetime
@@ -9,14 +9,17 @@ from pylogix import PLC
 app = Flask(__name__)
 app.secret_key = 'some_secret_key'
 
-# Global variable to control the script
+# Global variables to control the script
 script_running = False
 script_thread = None
+data_buffer = []
+tags = ["BLD01_PIT01_00.SMTH", "BLD01_PIT04_00.SMTH"]  # Default tags
 
-def run_trend_collection(device_number, trend_desc, cycles, cycle_time, buffer_size, plc_ip, tags):
-    global script_running
+
+def run_trend_collection(device_number, trend_desc, cycles, cycle_time, buffer_size, plc_ip):
+    global script_running, data_buffer
     script_running = True
-    
+
     col_tags = tags.copy()
     col_tags.insert(0, 'DateTime')
     col_tags.insert(0, 'Index')
@@ -32,8 +35,6 @@ def run_trend_collection(device_number, trend_desc, cycles, cycle_time, buffer_s
     with open(rel_path, 'w') as csv_file:
         csv_file = csv.writer(csv_file, delimiter=',', lineterminator='\n', quotechar='/', quoting=csv.QUOTE_MINIMAL)
         csv_file.writerow(col_tags)
-
-    data_buffer = []
 
     with PLC() as comm:
         comm.IPAddress = plc_ip
@@ -60,9 +61,11 @@ def run_trend_collection(device_number, trend_desc, cycles, cycle_time, buffer_s
 
     script_running = False
 
+
 @app.route('/')
 def index():
-    return render_template('index.html', script_running=script_running)
+    return render_template('index.html', script_running=script_running, tags=tags)
+
 
 @app.route('/start', methods=['POST'])
 def start_script():
@@ -75,26 +78,14 @@ def start_script():
         buffer_size = int(request.form['buffer_size'])
         plc_ip = request.form['plc_ip']
 
-        tags = ["BLD01_PIT01_00.SMTH", "BLD01_PIT04_00.SMTH", "BLD01_PIT05_00.SMTH", "BLD01_PT21_00.SMTH", "BLD01_PT21_02.SMTH",
-                "BLD01_PT22_00.SMTH", "BLD01_PT22_02.SMTH", "BLD01_PIT00_00.SMTH", "BLD01_P21_00_PV.SMTH", "BLD01_P22_00_PV.SMTH",
-                "BLD01_TT21_00.SMTH", "BLD01_TT21_01.SMTH", "BLD01_TT22_00.SMTH", "BLD01_TT22_01.SMTH", "BLD01_TT40_00.SMTH",
-                "BLD01_TT41_00.SMTH", "BLD01_TT42_00.SMTH", "BLD01_TT43_00.SMTH", "BLD01_TT44_00.SMTH", "BLD01_PT80_00.SMTH",
-                "BLD01_PT80_01.SMTH", "BLD01_FT80_00.SMTH", "BLD01_FT80_01.SMTH", "BLD01_PT40_00.SMTH", "BLD01_PT40_01.SMTH",
-                "BLD01_FT40_00.SMTH", "BLD01_FT40_01.SMTH", "BLD01_PT41_00.SMTH", "BLD01_PT41_01.SMTH", "BLD01_FT41_00.SMTH",
-                "BLD01_FT41_01.SMTH", "BLD01_PT42_00.SMTH", "BLD01_PT42_01.SMTH", "BLD01_FT42_00.SMTH", "BLD01_FT42_01.SMTH",
-                "BLD01_PT43_00.SMTH", "BLD01_PT43_01.SMTH", "BLD01_FT43_00.SMTH", "BLD01_FT43_01.SMTH", "BLD01_PT44_00.SMTH",
-                "BLD01_PT44_01.SMTH", "BLD01_FT44_00.SMTH", "BLD01_FT44_01.SMTH", "BLD01_AT21_01.SMTH", "BLD01_AT22_01.SMTH",
-                "BLD01_AT62_00.SMTH", "BLD01_AT63_00.SMTH", "BLD01_AT62_01.SMTH", "BLD01_AT63_01.SMTH", "BLD01_WT21_00.SMTH",
-                "BLD01_WT22_00.SMTH", "BLD01_AT21_00.SMTH", "BLD01_AT22_00.SMTH", "BLD01_AT40_00.SMTH", "BLD01_AT41_00.SMTH",
-                "BLD01_AT42_00.SMTH", "BLD01_AT43_00.SMTH", "BLD01_AT44_00.SMTH"]
-
-        script_thread = threading.Thread(target=run_trend_collection, args=(device_number, trend_desc, cycles, cycle_time, buffer_size, plc_ip, tags))
+        script_thread = threading.Thread(target=run_trend_collection, args=(device_number, trend_desc, cycles, cycle_time, buffer_size, plc_ip))
         script_thread.start()
         flash('Script started successfully!')
     else:
         flash('Script is already running!')
 
     return redirect(url_for('index'))
+
 
 @app.route('/stop', methods=['POST'])
 def stop_script():
@@ -106,6 +97,34 @@ def stop_script():
         flash('No script is running!')
 
     return redirect(url_for('index'))
+
+
+@app.route('/add_tag', methods=['POST'])
+def add_tag():
+    tags_input = request.form['tags']
+    new_tags = tags_input.splitlines()  # Split input by new lines
+
+    for tag in new_tags:
+        tag = tag.strip()  # Remove any leading/trailing whitespace
+        if tag and tag not in tags:
+            tags.append(tag)
+
+    return redirect(url_for('index'))
+
+
+
+@app.route('/remove_tag/<string:tag>', methods=['POST'])
+def remove_tag(tag):
+    if tag in tags:
+        tags.remove(tag)
+    return redirect(url_for('index'))
+
+
+@app.route('/data')
+def get_data():
+    global data_buffer
+    return jsonify(data_buffer[-10:])  # Return the last 10 rows of data
+
 
 if __name__ == '__main__':
     app.run(debug=True)
